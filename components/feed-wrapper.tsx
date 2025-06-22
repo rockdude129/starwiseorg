34a4131d03b0ea6f3e48b7b1809fc13f82da25e5
Cxsmxnaut <155774297+Cxsmxnaut@users.noreply.github.com>
2025-06-22 11:20:34 -0700

    Delete components directory

--- components/feed-wrapper.tsx
+++
@@ -1,5 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-export const FeedWrapper = ({ children }: PropsWithChildren) => {
-  return <div className="relative top-0 flex-1 pb-10">{children}</div>;
-};
